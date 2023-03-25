c7410c5c11873693475f0b167527f12213696054
and <andersonvittor1801@gmail.com>
2023-03-25 09:33:45 -0300

    Aula 13 Repetições Parte 1

---
+++ Aula 13 Repetições Parte 1/ambiente.js
@@ -0,0 +1,24 @@
+// Primeira Técnica
+let cont = 1
+while (cont <= 6){
+    console.log(`Passo ${cont}`)
+    cont++
+}
+
+/*
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+*/
+
+// Segunda técnica
+
+let conte = 1
+do {
+    console.log(`Passo ${conte}`)
+    conte++
+} while (conte <= 3)
