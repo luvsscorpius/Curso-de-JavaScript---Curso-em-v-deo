03c9072da1696daf90c8441824055fedd3046890
and <andersonvittor1801@gmail.com>
2023-03-26 11:27:41 -0300

    Aula 14 Repetições Parte 2

---
+++ Aula 14 Repetições Parte 2/ambiente.js
@@ -0,0 +1,16 @@
+/*
+let c = 1
+while (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+
+//
+
+// Primeira parte voce cria a variável, segunda voce faz o teste
+// lógico e se for verdadeiro voce executa o código.
+
+for (let c = 1; c <= 5; c++) {
+    console.log(c)
+}
