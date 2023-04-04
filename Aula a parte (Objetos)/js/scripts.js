a19211d7d70d723b396f2646c63904ea2b325412
and <andersonvittor1801@gmail.com>
2023-04-04 08:10:59 -0300

    Aula a parte (Objetos)

---
+++ Aula a parte (Objetos)/js/scripts.js
@@ -0,0 +1,12 @@
+let pessoa = {
+    nome: "Matheus",
+    Idade: 29,
+    falar: function () {
+        console.log(`Olá tudo bem, meu nome é`)
+    }
+}
+
+pessoa.falar()
+
+console.log(pessoa.nome)
+
